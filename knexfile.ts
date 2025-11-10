bc94cded4ec94ac3c1db74344b1e21d8e55cfeca
Maulana Shalihin <maulanashalihin@Maulanas-Mac-mini.local>
2025-11-10 18:21:57 +0800

    build

--- knexfile.ts
+++ knexfile.ts
@@ -14,7 +14,7 @@ const config: { [key: string]: Knex.Config } = {
   production: {
     client: "better-sqlite3",
     connection: {
-      filename: "./production.sqlite3"
+      filename: "../production.sqlite3"
     },
     useNullAsDefault : true
   },
